609d7b694804083a80a1af2930d08f1e41f2f852
Yesenia Juarez <yesejuarez16@gmail.com>
2023-05-16 17:41:51 -0600

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,30 @@ export class InfoPaginaService {
   info: InfopPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.CargarEquipo();
+  }
 
-    //console.log('Servicio de infoPagina listo')
-
+  private cargarInfo(){
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfopPagina) => {
+    .subscribe( (resp: InfopPagina) => {
+      this.cargada = true;
+      this.info = resp;
 
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
+      });
+  }
 
-        });
+  private CargarEquipo(){
+    //Leer el archivo JSON
+    this.http.get('https://angular-html-c7b7f-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe((resp: any) => {
+      this.equipo = resp;
+      console.log(resp);
 
-   }
+   } );
+  }
 }
